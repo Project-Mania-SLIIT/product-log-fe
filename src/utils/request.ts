8e48c74eab0b6db2ff84fdbd73e59df23190adfe
Pasindu Lakshan Gunasekara <shanugunasekara11@gmail.com>
2024-04-23 15:27:03 +0530

    changes be url

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Request = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://3.6.150.196:5000",
   headers: {
     "Content-Type": "application/json",
   },
